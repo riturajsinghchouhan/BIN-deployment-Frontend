f5d84ab0a003f6dc47d4fc182a3a5885daab5079
Ruturajsinghchouhan <riturajsinghchouhan007@gmail.com>
2026-03-17 01:53:35 +0530

    fasd

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://bins-deployment-backend.onrender.com/api',
+    baseURL: 'https://bins-deployment-backend.onrender.com',
 
     headers: {
         'Content-Type': 'application/json',
